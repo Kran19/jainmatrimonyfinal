c5f23bcb36c705ad8d729c3c6d376edb4232f8db
Tushar-Borgaonkar <tusharborgaonkar1@gmail.com>
2026-08-06 17:55:00 +0530

    feat: add custom header template for email notifications

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
+    <span style="font-size: 20px; font-weight: 800; color: #3730a3; letter-spacing: -0.5px;">
+        🙏 Jain Digambar Matrimony
+    </span>
+</a>
+</td>
+</tr>
